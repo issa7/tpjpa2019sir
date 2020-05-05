e4066c635f6dc0e94a2863984c431dbd2ca60a4c
issa7 <issa.keita.etudiant.rennes1@gmail.com>
2020-05-05 12:54:57 +0200

    dao

--- src/main/java/dao/ParticipantDao.java
+++ src/main/java/dao/ParticipantDao.java
@@ -6,7 +6,6 @@ import javax.ws.rs.core.MediaType;
 
 public class ParticipantDao {
 	@GET
-    @Produces(MediaType.TEXT_PLAIN)
     public String sayHello() {
         return "Hello, how are you?";
     }
